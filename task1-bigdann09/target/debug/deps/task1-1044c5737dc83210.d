26ca84dd7cc0553f6d315167e6d7dee2927fe4ff
Dann <etimdaniel08@gmail.com>
2025-09-18 18:21:27 +0100

    Deployed task1

---
+++ task1-bigdann09/target/debug/deps/task1-1044c5737dc83210.d
@@ -0,0 +1,5 @@
+/home/dann/Documents/projects/ackee-sos7/task1-bigdann09/target/debug/deps/libtask1-1044c5737dc83210.rmeta: src/main.rs
+
+/home/dann/Documents/projects/ackee-sos7/task1-bigdann09/target/debug/deps/task1-1044c5737dc83210.d: src/main.rs
+
+src/main.rs:
---
+++ task1-bigdann09/target/debug/deps/task1-b96d4b5d744b1edd.d
@@ -0,0 +1,5 @@
+/home/dann/Documents/projects/ackee-sos7/task1-bigdann09/target/debug/deps/libtask1-b96d4b5d744b1edd.rmeta: src/main.rs
+
+/home/dann/Documents/projects/ackee-sos7/task1-bigdann09/target/debug/deps/task1-b96d4b5d744b1edd.d: src/main.rs
+
+src/main.rs:
